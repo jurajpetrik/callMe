85f63d5395e06adaba32f2b4877f0ccc03c93b7e
Juraj Petrik <jurajpetrik77@gmail.com>
2014-05-17 19:32:19 -0300

    trying to display the stream in an audio element

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/client.html');
 });
 
+
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  socket.on('sound', function (stream) {
+    console.log('receiving data '+data);
+    socket.emit('sound',stream);
   });
 });
