1b7064c498f62547a62b8ed39cfabc8b024baeed
Juraj Petrik <jurajpetrik77@gmail.com>
2014-06-29 15:19:59 -0300

    removed unnecessary code

--- sound.js
+++ sound.js
@@ -1,36 +1,9 @@
-  window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              window.oRequestAnimationFrame      ||
-              window.msRequestAnimationFrame     ||
-              function(callback, element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-
-
     // Global Variables for Audio
     var audioContext;
 
     var sourceNode;
-    var analyserNode;
     var javascriptNode;
-    var playbackSourceNode;
     var audioStream;
-    var array = [];
-
-    var recording = null;  // this is the cumulative buffer for your recording
-
-    var audioBufferNode = null;
-    var audioBuffer = null;
-
-    // Global Variables for Drawing
-    var x = 0;
-    var canvasWidth  = 800;
-    var canvasHeight = 256;
-    var ctx;
-
 
     window.craicAudioContext = (function(){
       return  window.webkitAudioContext || window.AudioContext ;
@@ -43,12 +16,9 @@
 
     $(document).ready(function() {
      
-        socket.on('audio',addSampleToRecording);
+        socket.on('audio',playSound);
 
-
-        // Check that the browser can handle web audio
         try {
-//            audioContext = new webkitAudioContext();
             audioContext = new craicAudioContext();
 
         }
@@ -58,15 +28,12 @@
 
         // get the input audio stream and set up the nodes
         try {
-            // calls the function setupAudioNodes
-//            navigator.webkitGetUserMedia({audio:true}, setupAudioNodes, onError);
             navigator.getMedia({audio:true}, setupAudioNodes, onError);
 
         } catch (e) {
             alert('webkitGetUserMedia threw exception :' + e);
         }
 
-        // Start recording by setting onaudioprocess to the function that manages the recording buffer
         $("body").on('click', "#start_button",function(e) {
             e.preventDefault();
 
@@ -81,19 +48,6 @@
         $("body").on('click', "#stop_button",function(e) {
             e.preventDefault();
             javascriptNode.onaudioprocess = null;
-         });
-
-        // Play the recording
-        $("body").on('click', "#playback_button",function(e) {
-            e.preventDefault();
-            playRecording();
-         });
-
-        // Reset the recording buffer and the graphics, but keep the nodes connected
-        $("body").on('click', "#reset_button",function(e) {
-            e.preventDefault();
-            recording = null;
-            clearCanvas();
          });
 
         // Disable audio completely
@@ -125,35 +79,15 @@
         // connect the nodes together
         sourceNode.connect(javascriptNode);
         javascriptNode.connect(audioContext.destination);
-
-        // optional - connect input to audio output (speaker)
-        // This will echo your input back to your speakers - Beware of Feedback !!
-        // sourceNode.connect(audioContext.destination);
-
-        // allocate the array for Frequency Data
-        array = new Uint8Array(analyserNode.frequencyBinCount);
     }
 
-    // Add this buffer to the recording
-    // recording is a global
-    function addSampleToRecording(currentBuffer) {
-        playRecording(new Float32Array(currentBuffer));
-        /*if (recording ==  null) {
-            // handle the first buffer
-            recording = currentBuffer;
-        } else {
-            // allocate a new Float32Array with the updated length
-            newlen = recording.length + currentBuffer.length;
-            var newBuffer = new Float32Array(newlen);
-            newBuffer.set(recording, 0);
-            newBuffer.set(currentBuffer, recording.length);
-            recording = newBuffer;
-        }*/
-    }
 
-    function playRecording(recording) {
-        // You need to create the buffer node every time we play the sound
-        // Are we able to cleanup that memory or does the footprint grow over time ??
+    function playSound(receivedBuffer) {
+        var audioBufferNode = null;
+        var audioBuffer = null;
+
+        var recording = new Float32Array(receivedBuffer);
+
         if ( recording != null ) {
             // create the Buffer from the recording
             audioBuffer = audioContext.createBuffer( 1, recording.length, audioContext.sampleRate );
